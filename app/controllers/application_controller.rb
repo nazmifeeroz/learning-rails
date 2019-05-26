ef97add770dc7f40c56416bdefc2325c22f7f76b
Nazmi <nazmifeeroz@hotmail.com>
2019-05-26 13:26:55 +0800

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
+# Example app
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: 'hello, world!'
+  end
 end
